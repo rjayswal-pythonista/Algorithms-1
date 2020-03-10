b5ffb52eb28ae8c8bc6f93b88a78eae28366d017
Veerala Hari Krishna <veeralakrishna@gmail.com>
2020-03-10 19:53:18 +0530

    Add files via upload

---
+++ Palindrome Check/palindromeCheck_1.py
@@ -0,0 +1,8 @@
+
+# O(n^2) time | O(n) space
+
+def isPalindrome(string):
+	reversedString = ""
+	for i in reversed(range(len(string))):
+		reversedString += string[i]
+	return string == reversedString
